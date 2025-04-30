3623650e58007221b49e6eaed6d0ba2284b3cba3
Jiwon Kim <holiczsia@tamu.edu>
2025-04-30 00:50:06 -0500

    6th commit for server cert fetch

---
+++ full_build_run_takserver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd src
+
+./gradlew clean bootWar bootJar shadowJar || exit 1
+
+cd takserver-schemamanager
+
+../gradlew shadowJar || exit 1
+
+java -jar build/libs/schemamanager-5.2-RELEASE-16-uber.jar upgrade || exit 1
+
+cd ../takserver-core
+
+../gradlew clean bootWar bootJar || exit 1
+
+cd example/
+
+./run_takserver.sh
